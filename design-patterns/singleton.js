c7211b841d9473692e4d7fde9f14b41d4d4bb405
Alejandro Sánchez Pineda <alsapi2004@gmail.com>
2020-02-01 20:56:53 +0100

    singleton

---
+++ design-patterns/singleton.js
@@ -0,0 +1,53 @@
+/**
+ * Restricts instantiation of a class to a single object.
+ * Provide a shared namespace
+ */
+
+/**
+ * Module pattern
+ */
+function counter () {
+    let _privateCounter = 0;
+
+    // private method identified with beggining underscore as a convention
+    const _incrementCounter = () => _privateCounter++;
+    const _speakCounter = () => console.log('Counter is at', _privateCounter);
+    
+    // public method
+    function incrementCounter () {
+        _incrementCounter();
+        _speakCounter();
+    };
+
+    return {
+        incrementCounter
+    }
+}
+
+
+const Singleton = (function () {
+    let instance;
+
+    function getInstance() {
+        return instance || _init();
+    }
+
+    function _init(){
+        instance = counter();
+        return instance;
+    } 
+
+    return {getInstance};
+})();
+
+// rellay only one instance
+const firstInstance = Singleton.getInstance();
+const secondInstance = Singleton.getInstance();
+
+firstInstance.incrementCounter();
+secondInstance.incrementCounter();
+firstInstance.incrementCounter();
+secondInstance.incrementCounter();
+firstInstance.incrementCounter();
+secondInstance.incrementCounter();
+
